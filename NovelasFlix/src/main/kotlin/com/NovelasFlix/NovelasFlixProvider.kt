545de4883ac19ea0d7c7552531cfd2edf3a7f5aa
euluan1912 <euluan1912@gmail.com>
2025-11-27 21:34:17 -0300

    fix: Corrigida classe pai (Plugin) e import em NovelasFlixProvider.kt

--- NovelasFlix/src/main/kotlin/com/NovelasFlix/NovelasFlixProvider.kt
+++ NovelasFlix/src/main/kotlin/com/NovelasFlix/NovelasFlixProvider.kt
@@ -1,11 +1,15 @@
-package com.NovelasFlix
+ackage com.NovelasFlix
 
+import android.content.Context // <-- OBRIGATÓRIO!
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.Plugin // <-- CLASSE PAI CORRETA (Plugin, não BasePlugin)
 
 @CloudstreamPlugin
-class NovelasFlixProvider : BasePlugin() {
-    override fun load() {
+class NovelasFlixProviderPlugin: Plugin() {
+    
+    // A assinatura CORRETA para a função de carregamento
+    override fun load(context: Context) {
+        // Registra a API principal (NovelasFlix)
         registerMainAPI(NovelasFlix())
     }
-} 
+}

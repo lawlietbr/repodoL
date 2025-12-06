a91aefa603c834c42048c61d3e0d572a397eba54
L <euluan1912@gmail.com>
2025-12-06 09:37:43 -0300

    Atualizar o UltraCineProvider.kt

--- UltraCine/src/main/kotlin/com/UltraCine/UltraCineProvider.kt
+++ UltraCine/src/main/kotlin/com/UltraCine/UltraCineProvider.kt
@@ -9,3 +9,15 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 // ou apenas use o nome da classe, dependendo da configuração.
 import com.UltraCine.PlayEmbedApiSite
 import com.UltraCine.EmbedPlayExtractor
+
+@CloudstreamPlugin
+class UltraCineProvider : BasePlugin() {
+    override fun load() {
+        registerMainAPI(UltraCine()) 
+        
+        // Registro dos Extratores (Use o nome exato das classes)
+        registerExtractorAPI(PlayEmbedApiSite())
+        registerExtractorAPI(EmbedPlayExtractor()) 
+    }
+}
+
